9bceb9912c85e18758a4e2018d4bffd2509350a1
AbdoMossad <smsm22online@gmail.com>
2025-07-17 15:20:00 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -89,7 +89,7 @@ function formatFileDate(date) {
 
 
   // 1. Launch browser and go to the report URL
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://lookerstudio.google.com/reporting/cf50c9e0-7f4b-4e12-9583-3c2476b5d45b', { waitUntil: 'networkidle2' });
 
